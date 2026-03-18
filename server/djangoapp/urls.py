a2b2dd253d52dc385428a3b21c66a109104ab25a
Bhagyashree <shrekfour96@gmail.com>
2026-03-18 00:25:37 -0400

    Added dealer views, reviews, and post review endpoints

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,11 @@ urlpatterns = [
      path('login/', TemplateView.as_view(template_name="index.html")),
 path(route='logout', view=views.logout_request, name='logout'),    # path for dealer reviews view
 path(route='get_cars', view=views.get_cars, name='getcars'),
+  path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+path(route='get_dealer/<int:dealer_id>', view=views.get_dealer_details, name='get_dealer_details'),
+path(route='get_dealer_reviews/<int:dealer_id>', view=views.get_dealer_reviews, name='get_dealer_reviews'),
+
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
